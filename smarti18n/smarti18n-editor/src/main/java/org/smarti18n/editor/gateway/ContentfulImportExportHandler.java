724639f52e2ec5f150d629be748c745d211ecd4a
Marc Bellmann <marc.bellmann@planyourtrip.travel>
2017-11-22 16:04:50 +0100

    ContentfulImportExportHandler

---
+++ smarti18n/smarti18n-editor/src/main/java/org/smarti18n/editor/gateway/ContentfulImportExportHandler.java
@@ -0,0 +1,114 @@
+package org.smarti18n.editor.gateway;
+
+import org.smarti18n.api.MessagesApi;
+import org.smarti18n.api.Project;
+import org.smarti18n.editor.components.FormWindow;
+import org.smarti18n.editor.components.IconButton;
+import org.smarti18n.editor.utils.I18N;
+
+import com.contentful.java.cda.CDAArray;
+import com.contentful.java.cda.CDAClient;
+import com.contentful.java.cda.CDAEntry;
+import com.contentful.java.cda.CDALocale;
+import com.contentful.java.cda.CDASpace;
+
+import com.vaadin.icons.VaadinIcons;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.Window;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Component
+public class ContentfulImportExportHandler implements ImportExportHandler {
+
+    private final MessagesApi messagesApi;
+
+    public ContentfulImportExportHandler(final MessagesApi messagesApi) {
+        this.messagesApi = messagesApi;
+    }
+
+    @Override
+    public String getName() {
+        return "Contenful";
+    }
+
+    @Override
+    public boolean hasImport() {
+        return true;
+    }
+
+    @Override
+    public boolean hasExport() {
+        return false;
+    }
+
+    @Override
+    public Window createImportWindow(final Project project) {
+        final FormWindow formWindow = new FormWindow(I18N.translate("smarti18n.editor.message-import.caption"));
+
+        final TextField spaceKeyField = new TextField(
+                I18N.translate("smarti18n.editor.import-export.contentful.space-key")
+        );
+        formWindow.addFormComponent(spaceKeyField);
+
+        final TextField accessTokenField = new TextField(
+                I18N.translate("smarti18n.editor.import-export.contentful.access-token")
+        );
+        formWindow.addFormComponent(accessTokenField);
+
+        final TextField contentTypeIdentifierField = new TextField(
+                I18N.translate("smarti18n.editor.import-export.contentful.content-type-identifier")
+        );
+        formWindow.addFormComponent(contentTypeIdentifierField);
+
+        final TextField keyIdentifierField = new TextField(
+                I18N.translate("smarti18n.editor.import-export.contentful.key-identifier")
+        );
+        formWindow.addFormComponent(keyIdentifierField);
+
+        final TextField valueIdentifierField = new TextField(
+                I18N.translate("smarti18n.editor.import-export.contentful.value-identifier")
+        );
+        formWindow.addFormComponent(valueIdentifierField);
+
+
+        final IconButton importButton = new IconButton(
+                I18N.translate("smarti18n.editor.message-import.import", ""),
+                VaadinIcons.UPLOAD,
+                event ->{
+                    final CDAClient cdaClient = CDAClient.builder()
+                            .setSpace(spaceKeyField.getValue())
+                            .setToken(accessTokenField.getValue())
+                            .build();
+
+                    final CDASpace cdaSpace = cdaClient.fetchSpace();
+                    final CDAArray all = cdaClient
+                            .fetch(CDAEntry.class)
+                            .withContentType(contentTypeIdentifierField.getValue())
+                            .all();
+                    all.entries().values().forEach(cdaEntry -> {
+                        for (final CDALocale cdaLocale : cdaSpace.locales()) {
+                            final Locale locale = new Locale(cdaLocale.code());
+
+                            cdaEntry.setLocale(cdaLocale.code());
+
+                            final String key = cdaEntry.getField(keyIdentifierField.getValue());
+                            final String value = cdaEntry.getField(valueIdentifierField.getValue());
+
+                            this.messagesApi.update(project.getId(), key, locale, value);
+                        }
+                    });
+                }
+        );
+        formWindow.addFormButtons(importButton);
+
+        return formWindow;
+    }
+
+    @Override
+    public Window createExportWindow(final Project project) {
+        throw new UnsupportedOperationException("No Contentful Export available!");
+    }
+}
