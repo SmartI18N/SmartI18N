cf27ff6812235275617696431f2c9f91444dfc94
Marc Bellmann <marc.bellmann@planyourtrip.travel>
2020-01-31 20:30:51 +0100

    spring boot update

--- smarti18n/smarti18n-admin/src/main/java/org/smarti18n/admin/AdminApplication.java
+++ smarti18n/smarti18n-admin/src/main/java/org/smarti18n/admin/AdminApplication.java
@@ -1,21 +1,17 @@
 package org.smarti18n.admin;
 
+import org.smarti18n.api.ApiExceptionHandler;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
-import org.smarti18n.api.ApiExceptionHandler;
 
 /**
  * @author Marc Bellmann &lt;marc.bellmann@googlemail.com&gt;
  */
 @SpringBootApplication(
-        exclude = org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class,
         scanBasePackages = {"org.smarti18n.admin", "org.smarti18n.vaadin"}
 )
-@EnableAutoConfiguration
 public class AdminApplication {
 
     public static void main(String[] args) throws Exception {
--- smarti18n/smarti18n-editor/src/main/java/org/smarti18n/editor/EditorApplication.java
+++ smarti18n/smarti18n-editor/src/main/java/org/smarti18n/editor/EditorApplication.java
@@ -1,19 +1,15 @@
 package org.smarti18n.editor;
 
+import org.smarti18n.api.ApiExceptionHandler;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
-import org.smarti18n.api.ApiExceptionHandler;
 
 /**
  * @author Marc Bellmann &lt;marc.bellmann@googlemail.com&gt;
  */
 @SpringBootApplication(
-        exclude = SecurityAutoConfiguration.class,
         scanBasePackages = {"org.smarti18n.editor", "org.smarti18n.vaadin"}
 )
 public class EditorApplication {
